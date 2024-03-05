ba4afc7759eaf26ea9ae68bd6e56af8c5885844d
Tony.Zhu <zhutg_tony@cn.ibm.com>
2024-03-05 15:26:22 +0800

    add MilvusServiceClient

---
+++ src/main/java/com/example/demo/controller/MilvusController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import io.milvus.client.MilvusServiceClient;
+import io.milvus.param.ConnectParam;
+import io.milvus.param.R;
+import io.milvus.param.collection.HasCollectionParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MilvusController {
+
+    private static MilvusServiceClient milvusClient = new MilvusServiceClient(ConnectParam.newBuilder()
+            .withHost("localhost")
+            .withPort(19530)
+            .build());
+
+    @GetMapping("/checkMilvusService")
+    public void checkMilvusService(){
+        // list collections
+        R<Boolean> response = milvusClient.hasCollection(
+                HasCollectionParam.newBuilder()
+                        .withCollectionName("hello_milvus")
+                        .build());
+
+        System.out.println("milvus status is : " + response.getStatus());
+        System.out.println("response : " + response.getMessage());
+
+//        System.out.println("good boy!!!");
+    }
+}
---
+++ src/main/java/com/example/demo/milvus/SimpleExample.java
@@ -0,0 +1,26 @@
+package com.example.demo.milvus;
+
+import io.milvus.client.MilvusServiceClient;
+import io.milvus.param.ConnectParam;
+import io.milvus.param.R;
+import io.milvus.param.collection.HasCollectionParam;
+
+public class SimpleExample {
+
+    private static MilvusServiceClient milvusClient = new MilvusServiceClient(ConnectParam.newBuilder()
+            .withHost("localhost")
+            .withPort(19530)
+            .build());
+
+    public static void main(String[] args) {
+        // list collections
+//        R<Boolean> response = milvusClient.hasCollection(
+//                HasCollectionParam.newBuilder()
+//                        .withCollectionName("hello_milvus")
+//                        .build());
+//
+//        System.out.println("milvus status is : " + response.getStatus());
+
+        System.out.println("good boy!!!");
+    }
+}
